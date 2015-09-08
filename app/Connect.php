b909736e9de117d91e8a5925731f42463551d222
Ricardo Loo <ricardol@microsoft.com>
2015-09-08 16:36:25 -0700

    Base structure of sample and initial php file

---
+++ app/Connect.php
@@ -0,0 +1,89 @@
+<!-- Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file. -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>O365 Connect sample</title>
+
+  <!-- Third party dependencies. -->
+  <link rel="stylesheet" href="https://appsforoffice.microsoft.com/fabric/1.0/fabric.css">
+  <link rel="stylesheet" href="https://appsforoffice.microsoft.com/fabric/1.0/fabric.components.css">
+  
+  <!-- App code. -->
+  <script src="scripts/config.js"></script>
+  <script src="scripts/app.js"></script>
+  <script src="controllers/mainController.js"></script>
+  <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body class="ms-Grid">
+  <div class="ms-Grid-row ng-scope">
+  <!-- App navigation bar markup. -->
+  <div class="ms-NavBar">
+    <ul class="ms-NavBar-items">
+      <li class="navbar-header">Office 365 Connect sample</li>
+      <li class="ms-NavBar-item ms-NavBar-item--right ng-hide"><i class="ms-Icon ms-Icon--x"></i> Disconnect</li>
+    </ul>
+  </div>
+
+  <!-- App main content markup. -->
+  <div class="ms-Grid-col ms-u-mdPush1 ms-u-md9 ms-u-lgPush1 ms-u-lg6">
+    <div ng-hide="main.isConnected">
+      <p class="ms-font-xl">Use the button below to connect to Office 365.</p>
+      <button class="ms-Button" ng-click="main.connect()">
+        <span class="ms-Button-label">Connect to Office 365</span>
+      </button>
+    </div>
+    <div class="ng-hide" ng-show="main.isConnected">
+      <h2 class="ms-font-xxl ms-fontWeight-semibold ng-binding">Hi, !</h2>
+      <p class="ms-font-xl">You're now connected to Office 365. Click the mail icon below to send a message from your account using the Office 365 unified API. </p>
+      <div class="ms-TextField">
+        <input class="ms-TextField-field ng-pristine ng-untouched ng-valid" ng-model="main.emailAddress">
+      </div>
+      <div class="icon-holder">
+        <a href="" ng-click="main.sendMail()"><i class="ms-Icon ms-Icon--mailSend big-icon"></i></a>
+      </div>
+      <div class="ng-hide" ng-show="main.requestSuccess &amp;&amp; main.requestFinished">
+        <p class="ms-font-m ms-fontColor-green ng-binding">Successfully sent an email to !</p>
+      </div>
+      <div class="ng-hide" ng-show="!main.requestSuccess &amp;&amp; main.requestFinished">
+        <p class="ms-font-m ms-fontColor-redDark">Something went wrong, couldn't send an email.</p>        
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+
+</html>
+
+<!--
+O365-Angular-Unified-API-Connect, https://github.com/OfficeDev/O365-Angular-Unified-API-Connect
+ 
+Copyright (c) Microsoft Corporation
+All rights reserved. 
+ 
+MIT License:
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+ 
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.    
+  
+-->
