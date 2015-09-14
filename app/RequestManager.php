f47e00f44d47f90ba79377e656672bf1cc4bee85
Ricardo Loo <ricardol@microsoft.com>
2015-09-14 14:38:17 -0700

    Testing on Azure without verifypeer option in curl

--- app/RequestManager.php
+++ app/RequestManager.php
@@ -30,7 +30,7 @@
             ));
             
             // Debug options
-            // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             // curl_setopt($curl, CURLOPT_PROXY, '127.0.0.1:8888');
             
             // Enable error reporting on curl
