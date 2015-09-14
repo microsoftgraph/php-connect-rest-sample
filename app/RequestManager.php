8e4382766d4b22eb716560a5bf9fd692279a85b9
Ricardo Loo <ricardol@microsoft.com>
2015-09-14 13:35:26 -0700

    Enabled error reporting on curl

--- app/RequestManager.php
+++ app/RequestManager.php
@@ -31,7 +31,10 @@
             
             // Debug options
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            // curl_setopt($curl, CURLOPT_PROXY, '127.0.0.1:8888');
+            curl_setopt($curl, CURLOPT_PROXY, '127.0.0.1:8888');
+            
+            // Enable error reporting on curl
+            curl_setopt($curl, CURLOPT_FAILONERROR, true);
             
             // Send the request & save response to a variable
             $response = curl_exec($curl);
