06742ee7a88ea67510ccad969b75b633ee6d6ab3
Ricardo Loo <ricardol@microsoft.com>
2015-09-14 15:31:38 -0700

    Bug fix: In Linux the URLs are case sensitive

--- app/AuthenticationManager.php
+++ app/AuthenticationManager.php
@@ -104,7 +104,7 @@
             $connectUrl .= $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
             
             // Get the full URL of the connect.php to send it to the logout endpoint
-            $connectUrl = substr($connectUrl, 0, strrpos( $connectUrl, '/') ) . '/connect.php';
+            $connectUrl = substr($connectUrl, 0, strrpos( $connectUrl, '/') ) . '/Connect.php';
  
             // Logout endpoint is in the form
             // https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=<full_url_of_your_start_page> 
