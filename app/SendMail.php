19e45597fb59a0ce8438139c28b191a34d408ca0
Ricardo Loo <ricardol@microsoft.com>
2015-09-15 13:34:04 -0700

    Took care of SendMail.php style errors and warnings

--- app/SendMail.php
+++ app/SendMail.php
@@ -18,8 +18,7 @@
  */
  
 //We store user name, id, and tokens in session variables
-if (session_status() == PHP_SESSION_NONE)
-{
+if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 require_once 'MailManager.php';
@@ -34,8 +33,12 @@ require_once 'MailManager.php';
   <title>O365 Connect sample</title>
 
   <!-- Third party dependencies. -->
-  <link rel="stylesheet" href="https://appsforoffice.microsoft.com/fabric/1.0/fabric.css">
-  <link rel="stylesheet" href="https://appsforoffice.microsoft.com/fabric/1.0/fabric.components.css">
+  <link 
+      rel="stylesheet" 
+      href="https://appsforoffice.microsoft.com/fabric/1.0/fabric.css">
+  <link 
+      rel="stylesheet" 
+      href="https://appsforoffice.microsoft.com/fabric/1.0/fabric.components.css">
   
   <!-- App code. -->
   <link rel="stylesheet" href="styles.css">
@@ -48,7 +51,11 @@ require_once 'MailManager.php';
     <div class="ms-NavBar">
     <ul class="ms-NavBar-items">
         <li class="navbar-header">Office 365 Connect sample</li>
-        <li class="ms-NavBar-item ms-NavBar-item--right" onclick="window.location.href='Disconnect.php'"><i class="ms-Icon ms-Icon--x"></i> Disconnect</li>
+        <li 
+            class="ms-NavBar-item ms-NavBar-item--right" 
+            onclick="window.location.href='Disconnect.php'">
+            <i class="ms-Icon ms-Icon--x"></i> Disconnect
+        </li>
     </ul>
     </div>
 
@@ -56,10 +63,19 @@ require_once 'MailManager.php';
     <form action="" method="post">
     <div class="ms-Grid-col ms-u-mdPush1 ms-u-md9 ms-u-lgPush1 ms-u-lg6">
     <div>
-        <h2 class="ms-font-xxl ms-fontWeight-semibold">Hi, <?=$_SESSION['given_name']?>!</h2>
-        <p class="ms-font-xl">You're now connected to Office 365. Click the mail icon below to send a message from your account using the Office 365 unified API. </p>
+        <h2 class="ms-font-xxl ms-fontWeight-semibold">
+            Hi, <?php echo $_SESSION['given_name']; ?>!
+        </h2>
+        <p class="ms-font-xl">
+            You're now connected to Office 365. Click the mail icon below to send a 
+            message from your account using the Office 365 unified API.
+        </p>
         <div class="ms-TextField">
-        <input class="ms-TextField-field" name="recipient" value="<?=$_SESSION['unique_name']?>">
+        <input 
+            class="ms-TextField-field" 
+            name="recipient" 
+            value="<?php echo $_SESSION['unique_name']; ?>"
+        >
         </div>
         <button class="ms-Button">
                 <span class="ms-Button-label">Send mail</span>
@@ -67,23 +83,30 @@ require_once 'MailManager.php';
         <div>
             <?php
                 // The user clicked the "Send mail" button 
-                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recipient']))
+            if ($_SERVER['REQUEST_METHOD'] === 'POST'
+                && isset($_POST['recipient'])
+            ) {
+                try
                 {
-                    try
-                    {
-                        MailManager::sendWelcomeMail($_POST['recipient']);
+                    MailManager::sendWelcomeMail($_POST['recipient']);
             ?>
-                        <p class="ms-font-m ms-fontColor-green">Successfully sent an email to <?=$_POST['recipient']?>!</p>
+                        <p 
+                            class="ms-font-m ms-fontColor-green">
+                            Successfully sent an email to 
+                            <?php echo $_POST['recipient']; ?>!
+                        </p>
             <?php
-                    } 
-                    catch (RuntimeException $e) 
-                    {
+                } 
+                catch (RuntimeException $e) 
+                {
             ?>
-                        <p class="ms-font-m ms-fontColor-redDark">Something went wrong, couldn't send an email.</p>
+                        <p 
+                            class="ms-font-m ms-fontColor-redDark">
+                            Something went wrong, couldn't send an email.
+                        </p>
             <?php            
-                    }
-            
                 }
+            }
             ?>
         
         </div>
@@ -97,7 +120,8 @@ require_once 'MailManager.php';
 <?php
 // *********************************************************
 //
-// O365-PHP-Unified-API-Connect, https://github.com/OfficeDev/O365-PHP-Unified-API-Connect
+// O365-PHP-Unified-API-Connect
+// https://github.com/OfficeDev/O365-PHP-Unified-API-Connect
 //
 // Copyright (c) Microsoft Corporation
 // All rights reserved.
