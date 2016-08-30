2fc5fde5e0cb74dadb6414c968193672022c7cc1
Ricardo Loo <ricardol@microsoft.com>
2016-08-30 12:14:01 -0700

    Changed the sendmail endpoint to its shorter version

--- src/Constants.php
+++ src/Constants.php
@@ -34,7 +34,7 @@ class Constants
     const TOKEN_ENDPOINT = '/oauth2/v2.0/token';
     const LOGOUT_ENDPOINT = '/oauth2/logout';
     const RESOURCE_ID = 'https://graph.microsoft.com';
-    const SENDMAIL_ENDPOINT = '/v1.0/me/microsoft.graph.sendmail';
+    const SENDMAIL_ENDPOINT = '/v1.0/me/sendmail';
     const RESOURCE_OWNER_DETAILS_ENDPOINT = '/v1.0/me';
     const SCOPES='openid offline_access user.read mail.send';
 }
