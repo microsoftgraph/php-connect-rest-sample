eecc9cec5b96e98a35de47c77f8404968f3ff627
Ricardo Loo <ricardol@microsoft.com>
2016-08-30 14:22:53 -0700

    Minimized the amount of scopes required for the app to work

--- src/Constants.php
+++ src/Constants.php
@@ -34,5 +34,5 @@ class Constants
     const TOKEN_ENDPOINT = '/oauth2/v2.0/token';
     const RESOURCE_ID = 'https://graph.microsoft.com';
     const SENDMAIL_ENDPOINT = '/v1.0/me/sendmail';
-    const SCOPES='openid offline_access user.read mail.send';
+    const SCOPES='openid profile mail.send';
 }
